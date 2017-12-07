cbfde999cac251e13a3aa94661b514fa62015773
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-12-07 07:44:17 +0100

    playing with different font

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,10 @@ const rewireLess = require('react-app-rewire-less');
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);  // change importing css to less
     config = rewireLess.withLoaderOptions({
-        modifyVars: {"@primary-color": "#00C1DE"},
+        modifyVars: {
+            "@primary-color": "#00C1DE",
+            "@font-family-no-number": "Raleway"
+        },
     })(config, env);
     config = injectBabelPlugin('transform-decorators-legacy', config);
     return config;
